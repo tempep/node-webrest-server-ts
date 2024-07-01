35f650f78583614a26a108baa7f82c8191e53fea
Fernando <ferjesusurd@gmail.com>
2024-07-01 10:05:39 -0500

    RestServer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
     public_path?: string;
+    routes: Router;
 }
 
 
@@ -12,17 +13,29 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options){
         this.port = options.port;
         this.publicPath = options.public_path || 'public';
+        this.routes = options.routes;
     }
 
     async start() {
 
+
+        // Middleware
+        this.app.use( express.json() ); // Convierte el raw a json
+        this.app.use( express.urlencoded({ extended: true })); // Convierte el x-www-form-urlencoded
+
+        // Routes
+        this.app.use( this.routes );
+
+
         // Public Folder
         this.app.use( express.static( this.publicPath ) );
 
+        // SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join( __dirname + `../../../${ this.publicPath }/index.html`);
             res.sendFile(indexPath);
